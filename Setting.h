af6f8bef260d20a331387b5c0d9a669ce41799eb
Iman <namdari.iman@gmail.com>
2017-10-27 01:24:07 +0330

    Init project.

---
+++ Setting.h
@@ -0,0 +1,14 @@
+//
+// Created by iman on 10/27/17.
+//
+
+#ifndef __SETTING_H__
+#define __SETTING_H__
+
+/// Number of flips per packet.
+const int flipCount = 4;
+
+/// Number of cycles need to pass a single flit.
+const int flipDuration = 2;
+
+#endif //__SETTING_H__
