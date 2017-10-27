53fc0b72573d32b98da89ab43f264b4e620de609
Iman <namdari.iman@gmail.com>
2017-10-27 23:59:56 +0330

    Reading differently.

--- src/Container.cpp
+++ src/Container.cpp
@@ -18,26 +18,21 @@ Container::Container(int size) : _size(size) {
 void Container::readFlows(const std::string &address) {
     std::ifstream fin;
     fin.open(address);
-    for (auto start : _mesh) {
-        int flowCount;
-        fin >> flowCount;
-        for (int j = 0; j < flowCount; ++j) {
-            int end, sendTime;
-            std::string typeStr;
-            fin >> end >> sendTime >> typeStr;
-            FlowType type;
-            if (typeStr == "RT")
-                type = FlowType::RT;
-            else if (typeStr == "NRT")
-                type = FlowType::NRT;
-            else
-                assert(false);
-            if (start != _mesh[end])
-                _flows.push_back(new Flow(start, _mesh[end], sendTime, type));
-            else
-                std::cout << "warning : start and end of the flow is equal!"
-                          << std::endl;
-        }
+    int start, end, sendTime;
+    std::string typeStr;
+    while (fin >> start >> end >> sendTime >> typeStr) {
+        FlowType type;
+        if (typeStr == "RT")
+            type = FlowType::RT;
+        else if (typeStr == "NRT")
+            type = FlowType::NRT;
+        else
+            assert(false);
+        if (start != end)
+            _flows.push_back(new Flow(_mesh[start], _mesh[end], sendTime, type));
+        else
+            std::cout << "warning : start and end of the flow is equal!"
+                      << std::endl;
     }
     fin.close();
 }
