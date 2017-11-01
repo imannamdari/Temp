0e3e8febb55ff881f5c2338cd0324968ffba2891
Iman <namdari.iman@gmail.com>
2017-11-01 15:27:13 +0330

    In directory from argument.

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include "dirent.h"
 #include <sstream>
+#include <cassert>
 
 #include "Handler.h"
 
@@ -31,19 +32,21 @@ std::string removeTxt(std::string str) {
     return res;
 }
 
-int main() {
+int main(int argc, char **argv) {
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
     DIR *dir;
     struct dirent *ent;
-    if ((dir = opendir("../res/in")) != nullptr) {
+    assert(argc == 2);
+    std::string dirName = argv[1];
+    if ((dir = opendir(dirName.c_str())) != nullptr) {
         while ((ent = readdir(dir)) != nullptr) {
             std::string name = ent->d_name;
             if (name != "." && name != "..") {
                 std::cout << name << " start" << std::endl;
                 std::string temp = removeTxt(name);
                 mkdir(("../res/out/" + removeTxt(name)).c_str(), 0777);
-                Handler *handler = new Handler("../res/in/" + name,
-                                               "../res/out/" + removeTxt(name) +
+                Handler *handler = new Handler(dirName + "/" + name,
+                                               "out/" + removeTxt(name) +
                                                "/" + removeTxt(name),
                                                getSize(name), 5);
                 handler->handle();
